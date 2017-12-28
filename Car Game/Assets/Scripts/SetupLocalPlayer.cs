1b0fd0f5d328df1cadc3fedf88fdbd267b2b546f
Daniel <danboy2k1@gmail.com>
2017-12-28 11:34:46 -0500

    Can drive cars around with lobby

---
+++ Car Game/Assets/Scripts/SetupLocalPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SetupLocalPlayer : NetworkBehaviour {
+
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (isLocalPlayer)
+        {
+            GetComponent<Drive>().enabled = true;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
